18f35b0561581e508ffef0a8f3cbc2b96fb7f54f
sayazdt <90677840+sayazdt@users.noreply.github.com>
2026-05-10 15:59:57 +0330

    /

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 
@@ -40,8 +41,8 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
 
-    // modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    // modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalTitles.innerHTML = this.querySelector("[data-testimonials-titles]").innerHTML;
     modalTime.innerHTML = this.querySelector("[data-testimonials-time]").innerHTML;
@@ -57,86 +58,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-// custom select variables
-// const select = document.querySelector("[data-select]");
-// const selectItems = document.querySelectorAll("[data-select-item]");
-// const selectValue = document.querySelector("[data-selecct-value]");
-// const filterBtn = document.querySelectorAll("[data-filter-btn]");
-
-// select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// // add event in all select items
-// for (let i = 0; i < selectItems.length; i++) {
-//   selectItems[i].addEventListener("click", function () {
-
-//     let selectedValue = this.innerText.toLowerCase();
-//     selectValue.innerText = this.innerText;
-//     elementToggleFunc(select);
-//     filterFunc(selectedValue);
-
-//   });
-// }
-
-// filter variables
-// const filterItems = document.querySelectorAll("[data-filter-item]");
-
-// const filterFunc = function (selectedValue) {
-
-//   for (let i = 0; i < filterItems.length; i++) {
-
-//     if (selectedValue === "all") {
-//       filterItems[i].classList.add("active");
-//     } else if (selectedValue === filterItems[i].dataset.category) {
-//       filterItems[i].classList.add("active");
-//     } else {
-//       filterItems[i].classList.remove("active");
-//     }
-
-//   }
-
-// }
-
-// add event in all filter button items for large screen
-// let lastClickedBtn = filterBtn[0];
-
-// for (let i = 0; i < filterBtn.length; i++) {
-
-//   filterBtn[i].addEventListener("click", function () {
-
-//     let selectedValue = this.innerText.toLowerCase();
-//     selectValue.innerText = this.innerText;
-//     filterFunc(selectedValue);
-
-//     lastClickedBtn.classList.remove("active");
-//     this.classList.add("active");
-//     lastClickedBtn = this;
-
-//   });
-
-// }
-
-
-
-// contact form variables
-// const form = document.querySelector("[data-form]");
-// const formInputs = document.querySelectorAll("[data-form-input]");
-// const formBtn = document.querySelector("[data-form-btn]");
-
-// // add event to all form input field
-// for (let i = 0; i < formInputs.length; i++) {
-//   formInputs[i].addEventListener("input", function () {
-
-//     // check form validation
-//     if (form.checkValidity()) {
-//       formBtn.removeAttribute("disabled");
-//     } else {
-//       formBtn.setAttribute("disabled", "");
-//     }
-
-//   });
-// }
+///////
 
 
 
@@ -161,3 +83,4 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
---
+++ assets/js/scriptp.js
@@ -0,0 +1,225 @@
+
+
+function he (){
+  document.write(`    
+  <meta charset="utf-8"> 
+  <meta content="width=device-width, initial-scale=1.0" name="viewport"> 
+  <title>Saber Malekzadeh</title>         
+  <meta content="Sabir Məlikzadə Saber MalekzadeH" name="description"> 
+  <meta content="Saber, Malekzadeh, Sabir, Məlikzadə, Malikzada, Melikzade, صابر ملک زاده " name="keywords">
+
+  <meta name="author" content="Saber Malekzadeh">
+
+  <meta property="og:type" content="website">
+  <meta name="robots" content="all, index">
+  <meta property="og:description" content="Sabir Məlikzadə صابر ملک زاده Saber Malekzadeh">
+  <meta property="og:locale" content="az_AZ">
+  <meta property="og:locale" content="us_US">
+  <meta property="og:locale" content="ir_IR">
+  <meta property="og:title" content="صابر ملک زاده">
+  <meta property="og:url" content="https://smalekz.github.io">
+  <meta property="og:site_name" content="Sabir Məlikzadə صابر ملک زاده Saber Malekzadeh">
+  <meta property="og:image" content="https://smalekz.github.io/assets/images/avatar.webp">
+  <meta property="og:image:secure_url" content="https://smalekz.github.io/assets/images/avatar.webp">
+  <meta property="og:url" content="https://smalekz.github.io">
+  
+  <style>
+  body {
+      font-family: 'Vazir';font-size: 22px;
+  }
+  </style>
+
+  <!--
+    - favicon
+  -->
+  <link href="assets/images/avatar.webp" rel="icon"> 
+  <link href="assets/images/avatar.webp" rel="apple-touch-icon"> 
+  <link rel="shortcut icon" href="assets/images/avatar.webp" type="image/x-icon">
+
+  <!--
+    - custom css link
+  -->
+  <link rel="stylesheet" href="./assets/css/style.css">
+
+    <!-- schema -->
+  <script type="application/ld+json">
+        {
+        "@context": "http://schema.org/",
+        "@type": "Person",
+        "name": "Saber Malekzadeh",
+        "url": "https://smalekz.github.io",
+        "logo": "https://smalekz.github.io/assets/images/avatar.webp", 
+        "image": "https://smalekz.github.io/assets/images/avatar.webp",
+        "telephone": "+989142232172",
+        "email": "smalekzp@gmail.com",
+        "@id": "https://smalekz.github.io",
+        "openingHoursSpecification": [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday"
+                  ],
+                  "opens": "9:00",
+                  "closes": "21:00"
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": [
+                    "Saturday",
+                    "Sunday"
+                  ],
+                  "opens": "10:00",
+                  "closes": "20:00"
+                }
+        ],
+        "geo": {
+            "@type": "GeoCoordinates",
+            "latitude": 40.424513
+            "longitude": 49.826117
+        },
+            
+         "address": {
+          "@type": "PostalAddress",
+          "streetAddress": "Etesami",
+          "addressLocality": "Tabriz",
+          "postalCode": "5157",
+          "addressCountry": "IR"
+         },
+          
+          "aggregateRating": {
+           "@type": "AggregateRating",
+           "ratingValue": "4.9",
+           "reviewCount": "93"
+          }, 
+          
+         
+          
+        "sameAs": [
+          "https://www.instagram.com/sabermalek/",
+          "https://www.linkedin.com/in/smalekzp/",
+          "http://smalekz.github.io"
+         ]
+        }
+        </script>
+
+      `);
+    
+
+      
+}
+
+function h1 (){
+    document.write(`    
+    <meta charset="utf-8"> 
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"> 
+    <title>صابر ملک زاده</title>         
+    <meta content="Sabir Məlikzadə Saber MalekzadeH" name="description"> 
+    <meta content="Saber, Malekzadeh, Sabir, Məlikzadə, Malikzada, Melikzade, صابر ملک زاده " name="keywords">
+  
+    <meta name="author" content="صابر ملک زاده">
+  
+    <meta property="og:type" content="website">
+    <meta name="robots" content="all, index">
+    <meta property="og:description" content="Sabir Məlikzadə صابر ملک زاده">
+    <meta property="og:locale" content="az_AZ">
+    <meta property="og:locale" content="us_US">
+    <meta property="og:locale" content="ir_IR">
+    <meta property="og:title" content="صابر ملک زاده">
+    <meta property="og:url" content="https://smalekz.github.io">
+    <meta property="og:site_name" content="Sabir Məlikzadə صابر ملک زاده">
+    <meta property="og:image" content="https://smalekz.github.io/assets/images/avatar.webp">
+    <meta property="og:image:secure_url" content="https://smalekz.github.io/assets/images/avatar.webp">
+    <meta property="og:url" content="https://smalekz.github.io">
+    
+    <style>
+    body {
+        font-family: 'Vazir';font-size: 22px;
+    }
+    </style>
+  
+    <!--
+      - favicon
+    -->
+    <link href="assets/images/avatar.webp" rel="icon"> 
+    <link href="assets/images/avatar.webp" rel="apple-touch-icon"> 
+    <link rel="shortcut icon" href="assets/images/avatar.webp" type="image/x-icon">
+  
+    <!--
+      - custom css link
+    -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+  
+      <!-- schema -->
+    <script type="application/ld+json">
+          {
+          "@context": "http://schema.org/",
+          "@type": "Person",
+          "name": "صابر ملک زاده",
+          "url": "https://smalekz.github.io",
+          "logo": "https://smalekz.github.io/assets/images/avatar.webp", 
+          "image": "https://smalekz.github.io/assets/images/avatar.webp",
+          "telephone": "+989142232172",
+          "email": "smalekzp@gmail.com",
+          "@id": "https://smalekz.github.io",
+          "openingHoursSpecification": [
+                  {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                      "Monday",
+                      "Tuesday",
+                      "Wednesday",
+                      "Thursday",
+                      "Friday"
+                    ],
+                    "opens": "9:00",
+                    "closes": "21:00"
+                  },
+                  {
+                    "@type": "OpeningHoursSpecification",
+                    "dayOfWeek": [
+                      "Saturday",
+                      "Sunday"
+                    ],
+                    "opens": "10:00",
+                    "closes": "20:00"
+                  }
+          ],
+          "geo": {
+              "@type": "GeoCoordinates",
+              "latitude": 40.424513
+              "longitude": 49.826117
+          },
+              
+           "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Etesami",
+            "addressLocality": "Tabriz",
+            "postalCode": "5157",
+            "addressCountry": "IR"
+           },
+            
+            "aggregateRating": {
+             "@type": "AggregateRating",
+             "ratingValue": "4.9",
+             "reviewCount": "93"
+            }, 
+            
+           
+            
+          "sameAs": [
+            "https://www.instagram.com/sabermalek/",
+            "https://www.linkedin.com/in/smalekzp/",
+            "http://smalekz.github.io"
+           ]
+          }
+          </script>
+  
+        `);
+      
+  
+        
+  }
